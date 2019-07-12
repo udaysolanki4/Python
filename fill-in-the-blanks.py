538543cd26a6aead102d8ebf5d6909c8d7bfea33
Uday Singh Solanki <37234643+udaysolanki4@users.noreply.github.com>
2019-07-12 12:42:57 +0530

    Update fill-in-the-blanks.py

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,3 +1,18 @@
+# Lesson 3.1:
+# Mini-Project: fill in the blank game
+
+# Game has 3 or more levels and each level contains 4 or more blanks to fill in
+# Immediately after running the program, user is prompted to select a difficulty level from easy / medium / hard
+
+# Once a level is selected, game displays a fill-in-the-blank and a prompt to fill in the first blank.
+#When player guesses correctly, new prompt shows with correct answer in the previous blank and a new prompt for the next blank
+#
+#When player guesses incorrectly, they are prompted to try again
+#
+
+# Your code here.
+
+
 s1=["The bus stand is directly _________ the police station.",'behind']
 s2=["As the doctor __________ into the room, the nurse handed him the temperature chart of the patient.The court _______ cognisance of the criminal's words.",'came','took']
 s3=["The improvement made by changes in the system was ________ and did not warrant the large expenses.She takes delight _________ teasing boys.",'marginal','in']
