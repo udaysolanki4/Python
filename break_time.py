3bcc93a3944c7481cd658c6930a6bb1cbcb7290b
Uday Singh Solanki <37234643+udaysolanki4@users.noreply.github.com>
2019-09-15 16:27:57 +0530

    Update break_time.py

--- break_time.py
+++ break_time.py
@@ -1,16 +1,3 @@
-# Lesson 3.2:
-# Mini-Project: Take a Break
-
-# Write a program that prompts the user to take a break
-# once every two hours, a maximum of three times.
-
-# Use this space to describe your approach to the problem.
-#open browser
-#time delay
-#repetition fo 3 times a day
-#
-
-# Your code here.
 
 import time
 import webbrowser
