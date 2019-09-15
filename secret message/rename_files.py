c181ec15de2451fe9db191f16841720ba3872fe6
Uday Singh Solanki <37234643+udaysolanki4@users.noreply.github.com>
2019-09-15 16:28:31 +0530

    Update rename_files.py

--- secret message/rename_files.py
+++ secret message/rename_files.py
@@ -1,14 +1,4 @@
-# Lesson 3.2: Use Functions
-# Mini-Project: Secret Message
 
-# Your friend has hidden your keys! To find out where they are,
-# you have to remove all numbers from the files in a folder
-# called prank. But this will be so tedious to do!
-# Get Python to do it for you!
-
-
-
-# Your code here.
 import os
 
 def rename1():
